7f9dce2aa38f967736dd9bb50fe10722b05d9e0f
PiGru08 <grucar77@gmail.com>
2024-09-05 08:32:12 +0200

    instrukcja switch

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witaj ponownie świecie");
-        int wylosowana = (int)(Math.random()*10+1);
+        int wylosowana = (int)(Math.random()*100+1);
 
         System.out.println(wylosowana);
         /*
@@ -16,15 +16,35 @@ public class Main {
         typy złożone: pisane zawsze pisane wielką literą
         obiekty które mają własności i metody
          */
-        System.out.println("Zgadnij liczbę z zakresu 1-10");
+        System.out.println("Zgadnij liczbę z zakresu 1-100");
         Scanner klawiatura = new Scanner(System.in);
         int wpisanaLiczba = klawiatura.nextInt();//wczytywanie z klawiatury liczby całkowitej
         System.out.println("Wpisano z klawiatury "+wpisanaLiczba);
-
+        //instrukcja warunkowa
         if(wpisanaLiczba == wylosowana){
             System.out.println("Liczba została odgadnięta.");
         }else{
             System.out.println("Nie zgadnięto, spróbuj jeszce raz.");
+        }
+        //wyrażenie warunkowe
+        String komunikat = wylosowana == wpisanaLiczba ? "Poprawnie":"Nie poprawnie";//jeżeli wylosowana jest taka sama jak wpisana to wypisz poprawnie, w przeciwnym wypadku nie poprawnie
+        System.out.println(komunikat);
+
+        int roznica = wylosowana>wpisanaLiczba ? wylosowana - wpisanaLiczba : wpisanaLiczba-wylosowana;
+        roznica = roznica/10;//dzielenie całkowite bo wynik typu int(całkowity)
+        switch (roznica){
+            case 0:
+                System.out.println("Brawo");
+                break;
+            case 1 :
+                System.out.println("Bardzo blisko");
+                break;
+            case 2:
+                System.out.println("Dość blisko");
+                break;
+            default:
+                System.out.println("Spróbuj szczęścia innym razem");
+                break;
         }
     }
 }
